f257583a9c4fb318ab07f09b998740a863e05aaf
Arsyian Rizki Pratama <81981819+ArsyianRizkiPratama@users.noreply.github.com>
2022-12-16 16:28:26 +0700

    folder view & program list buku
    Menambah folder view yang berisi program dart list buku

---
+++ view/book_list_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class BookListPage extends StatefulWidget {
+  const BookListPage({super.key});
+
+  @override
+  State<BookListPage> createState() => _BookListPageState();
+}
+
+class _BookListPageState extends State<BookListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //appBar untuk menampilkan bar paling atas pada app
+      appBar: AppBar(
+        title: Text("Book Catalogue"),
+      ),
+    );
+  }
+}
