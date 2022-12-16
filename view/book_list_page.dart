8f8de77f4866f8892838ee168f9763b7539f4a79
Arsyian Rizki Pratama <81981819+ArsyianRizkiPratama@users.noreply.github.com>
2022-12-16 16:50:51 +0700

    comment  print(await http.read......
    //print(await http.read(Uri.https('example.com', 'foobar.txt')));

--- view/book_list_page.dart
+++ view/book_list_page.dart
@@ -22,7 +22,7 @@ class _BookListPageState extends State<BookListPage> {
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
 
-    print(await http.read(Uri.https('example.com', 'foobar.txt')));
+    //print(await http.read(Uri.https('example.com', 'foobar.txt')));
   }
   
   @override
