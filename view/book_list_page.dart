26da52b63098e5732dfb422161bf73d96fe99d51
Arsyian Rizki Pratama <81981819+ArsyianRizkiPratama@users.noreply.github.com>
2022-12-16 16:48:44 +0700

    menambahkan 'async'
    menambah async pada fungsi fectBookAPI karena 'await' merupakan fungsi asyncronus

--- view/book_list_page.dart
+++ view/book_list_page.dart
@@ -14,13 +14,15 @@ class _BookListPageState extends State<BookListPage> {
   
    //membuat fungsi untuk bukunya
   //link untuk isi fungsi di bawah ("https://pub.dev/packages/http")
-  fectBookApi(){
-var url = Uri.https('example.com', 'whatsit/create');
-var response = await http.post(url, body: {'name': 'doodle', 'color': 'blue'});
-print('Response status: ${response.statusCode}');
-print('Response body: ${response.body}');
+  // karena await merupakan 'await' pasti merupakan fungsi asyncronus, maka perlu di tambahkan async setelah fectBookApi()
+  fectBookApi() async {
+    var url = Uri.https('example.com', 'whatsit/create');
+    var response =
+        await http.post(url, body: {'name': 'doodle', 'color': 'blue'});
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
 
-print(await http.read(Uri.https('example.com', 'foobar.txt')));
+    print(await http.read(Uri.https('example.com', 'foobar.txt')));
   }
   
   @override
