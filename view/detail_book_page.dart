365d115d338781a9bd494dbb22975a6d12833e25
Arsyian Rizki Pratama <81981819+ArsyianRizkiPratama@users.noreply.github.com>
2022-12-19 02:58:43 +0700

    Halaman Detail Book
    Halaman detail buku

---
+++ view/detail_book_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class DetailBookPage extends StatefulWidget {
+  const DetailBookPage({super.key});
+
+  @override
+  State<DetailBookPage> createState() => _DetailBookPageState();
+}
+
+class _DetailBookPageState extends State<DetailBookPage> {
+  fecthDetailBookApi() async {
+    var url = Uri.parse('https://api.itbook.store/1.0/books/9781484206485');
+    var response =
+        await http.post(url, body: {'name': 'doodle', 'color': 'blue'});
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    //print(await http.read(Uri.https('example.com', 'foobar.txt')));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Detail"),
+      ),
+    );
+  }
+}
